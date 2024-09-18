3fe9c60b7b695ec275204eb1248ce03b813de3c7
jen067 <dudu671194@gmail.com>
2024-09-18 22:04:01 +0800

    circle animation

--- intro-page/script.js
+++ intro-page/script.js
@@ -1,12 +1,14 @@
 let imgBeChanged = document.querySelector(".change");
 let colorBeChanged = document.querySelector(".main-title");
 let txtWrapper = document.querySelector(".txt-wrapper");
+let circle = document.querySelector(".circle");
+let body = document.querySelector("body");
 function changeImg(newSrc) {
   imgBeChanged.classList.add("fade-out");
   setTimeout(() => {
     imgBeChanged.src = newSrc;
     imgBeChanged.classList.remove("fade-out");
-  }, 500);
+  }, 1500);
 }
 
 function changeTxtColor(newClr) {
@@ -14,5 +16,17 @@ function changeTxtColor(newClr) {
   setTimeout(() => {
     colorBeChanged.style.color = newClr;
     txtWrapper.classList.remove("fade-out");
-  }, 500);
+  }, 1000);
+}
+console.log("h");
+function circleGrow(newclr) {
+  console.log("h");
+  circle.style.background = newclr;
+  circle.classList.add("grow");
+  body.style.overflow = "hidden";
+  circle.addEventListener("transitionend", () => {
+    console.log("f");
+    circle.classList.remove("grow");
+    body.style.overflow = "visible";
+  });
 }
