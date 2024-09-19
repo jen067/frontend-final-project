2be643fe9f052ce5d9b1acef7067ef37eb29c46a
jen067 <dudu671194@gmail.com>
2024-09-19 20:30:55 +0800

    圖片預先加載

--- intro-page/script.js
+++ intro-page/script.js
@@ -1,3 +1,20 @@
+// 需要給一個記憶體空間 imgPreloadArr 暫存
+const imgPreloadArr = [];
+// 前端緩存函式
+function frontendPreload() {
+  // 若有 4 張圖，用 for 迴圈遍歷
+  for (let i = 0; i < 3; i++) {
+    // 步驟 1. 建立實體化圖片
+    const img = new Image();
+    // 步驟 2. 給予 src 屬性，這裡要替換成你的圖片路徑
+    img.src = `{img/img1.png,img/img2.png,img/img3.png}`;
+    // 步驟 3. 緩存放在記憶體
+    imgPreloadArr.push(img);
+  }
+}
+// 執行該函式
+frontendPreload();
+
 let imgBeChanged = document.querySelector(".change");
 let colorBeChanged = document.querySelector(".main-title");
 let txtWrapper = document.querySelector(".txt-wrapper");
