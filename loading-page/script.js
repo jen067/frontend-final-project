a1aaf39d00d13ca8ba72021e05ccb5d1ad7ef07b
jen067 <dudu671194@gmail.com>
2024-09-20 00:03:08 +0800

    加入切換頁面時的過場動畫及頁面跳轉

---
+++ loading-page/script.js
@@ -0,0 +1,10 @@
+const fish = document.querySelector(".fish");
+
+window.onload = function () {
+  setTimeout(() => fish.classList.add("move"), 500);
+};
+
+setTimeout(
+  "location.href='https://jen067.github.io/frontend-final-project/intro-page/index.html'",
+  3000
+);
