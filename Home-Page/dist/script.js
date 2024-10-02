fe693e0a0b944ca977878ccb54ea365361372f98
jen067 <dudu671194@gmail.com>
2024-10-02 21:10:57 +0800

    圖片暫存設置

--- Home-Page/dist/script.js
+++ Home-Page/dist/script.js
@@ -1,3 +1,26 @@
+// 需要給一個記憶體空間 imgPreloadArr 暫存
+const imgPreloadArr = [];
+// 前端緩存函式
+function frontendPreload() {
+  // 用一個陣列存放所有要預加載的圖片路徑
+  const imgSrcArr = [
+    "img/IMG_0322.PNG",
+    "img/IMG_0323.PNG",
+    "img/IMG_0324.PNG",
+    "img/IMG_0325.png",
+  ];
+
+  // 使用 for 迴圈遍歷每張圖片路徑
+  for (let i = 0; i < imgSrcArr.length; i++) {
+    // 步驟 1. 建立實體化圖片
+    const img = new Image();
+    // 步驟 2. 給予 src 屬性，這裡使用的是當前遍歷的圖片路徑
+    img.src = imgSrcArr[i];
+    // 步驟 3. 將已緩存的圖片存放在記憶體中
+    imgPreloadArr.push(img);
+  }
+}
+
 let listBg = document.querySelectorAll(".bg");
 let listTab = document.querySelectorAll(".tab");
 let titleBanner = document.querySelector(".banner h1");
